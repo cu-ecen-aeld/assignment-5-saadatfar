d90b976a6f8a052672edf955e247979bb91e075d
Mohammad Hossein Saadatfar <mh.saadatfar@gmail.com>
2023-02-05 18:18:58 +0330

    Implement assignment 9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 238366970b30fc9b81395cf8e32823729a06d526
+AESD_ASSIGNMENTS_VERSION = 2f25512d21028d9a307b831411f5a07da0543ab9
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
